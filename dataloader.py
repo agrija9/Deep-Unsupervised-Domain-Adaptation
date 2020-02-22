a9c0b094e3cb3db95b27f782c4ea4c6c3a97dae1
alan <agrija9@gmail.com>
2020-02-22 18:33:11 +0100

    added dataloader (testing)

---
+++ dataloader.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Saturday Feb 22 2020
+
+@authors: Alan Preciado, Santosh Muthireddy
+"""
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, datasets
+from torch.utils.data.sampler import SubsetRandomSampler
+from PIL import Image
+
+
+class OfficeAmazonDataset(Dataset):
+    """Class to create an iterable dataset 
+    of images and corresponding labels """
+        
+    def __init__(self, image_folder_dataset, transform=None):
+        super(OfficeAmazonDataset, self).__init__()
+        self.image_folder_dataset = image_folder_dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_folder_dataset.imgs)
+    
+    def __getitem__(self, idx):
+        # read image, class from folder_dataset given index
+        img, img_label = image_folder_dataset[idx][0], image_folder_dataset[idx][1]
+        
+        # apply transformations (it already returns them as torch tensors)
+        if self.transform is not None:
+            self.transform(img)
+        
+        img_label_pair = {"image": img,
+                         "class": img_label}
+        
+        return img_label_pair
+
+
+def get_dataloader(dataset, batch_size):
+        
+    def get_subset(indices, start, end):
+        return indices[start:start + end]
+    
+    # Split train/val data ratios
+    TRAIN_RATIO, VALIDATION_RATIO = 0.7, 0.3
+    train_set_size = int(len(dataset) * TRAIN_RATIO)
+    validation_set_size = int(len(dataset) * VALIDATION_RATIO)
+
+    # Generate random indices for train and val sets
+    indices = torch.randperm(len(dataset))
+    train_indices = get_subset(indices, 0, train_set_size)
+    validation_indices = get_subset(indices, train_set_size, validation_set_size)
+    # test_indices = get_subset(indices, train_count + validation_count, len(dataset))
+
+    # Create sampler objects 
+    train_sampler = SubsetRandomSampler(train_indices)
+    val_sampler = SubsetRandomSampler(validation_indices)
+
+    # Create data loaders for data
+    train_loader = DataLoader(dataset, batch_size=batch_size, 
+                              sampler=train_sampler, num_workers=4)
+
+    val_loader = DataLoader(dataset, batch_size=batch_size, 
+                            sampler=val_sampler, num_workers=4)
+    
+    return train_loader, val_loader
--- models/alexnet.py
+++ models/alexnet.py
@@ -23,7 +23,7 @@ class AlexNet(nn.Module):
 			nn.Conv2d(in_channels=64, out_channels=192, kernel_size=5, padding=2),
 			nn.ReLU(inplace=True),
 			nn.MaxPool2d(kernel_size=3, stride=2),
-			nn.Conv2d(192, 384, kernel_size=3, padding=1),
+			nn.Conv2d(in_channels=192, out_channels=384, kernel_size=3, padding=1),
 			nn.ReLU(inplace=True),
 			nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),
 			nn.ReLU(inplace=True),
